eca54120c63cadb3ab93d915a3a62f8d8b898bf0
Najmul Huda <najmul.myself@gmail.com>
2021-12-10 01:25:47 +0600

    coffee app ingredients

--- coffee/lib/main.dart
+++ coffee/lib/main.dart
@@ -15,25 +15,92 @@ class MyApp extends StatelessWidget {
           color: Color(0xff201520),
           child: ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  width: double.infinity,
-                  height: 300,
-                  // child: Image.asset(
-                  //   "assets/images/coffee.png",
-                  //   fit: BoxFit.cover,
-                  // ),
-                  decoration: BoxDecoration(
-                    // color: Colors.amber,
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/coffee.png"),
-                      fit: BoxFit.cover,
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 400,
+                    // child: Image.asset(
+                    //   "assets/images/coffee.png",
+                    //   fit: BoxFit.cover,
+                    // ),
+                    decoration: BoxDecoration(
+                      // color: Colors.amber,
+                      image: const DecorationImage(
+                        image: AssetImage("assets/images/coffee.png"),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(18),
                     ),
-                    borderRadius: BorderRadius.circular(18),
                   ),
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Cappuccino",
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Drizzled with Caramel",
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 8.0,
+                              left: 38.0,
+                            ),
+                            child: Icon(
+                              Icons.star,
+                              size: 15,
+                              color: Colors.amber,
+                            ),
+                          ),
+                          Text(
+                            "4.5",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "A single espresso shot poured into hot foamy milk, with the surface topped with mildly sweetened cocoa powder and drizzled with scrumptious caramel syrup ... Read More",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 18.0),
+                      child: Text(
+                        "Choice of Milk",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
