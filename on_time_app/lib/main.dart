b7b965083025c0a810870f951ade9397e4240f24
Najmul Huda <najmul.myself@gmail.com>
2021-12-10 23:29:37 +0600

    ontime app

---
+++ on_time_app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        backgroundColor: Color(0xff0E091A),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('on.time'),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.notifications),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.more_vert),
+            ),
+          ],
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
