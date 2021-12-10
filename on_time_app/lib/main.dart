5998de2b56d70fe061fb76ad9d250972d3e7f85c
Najmul Huda <najmul.myself@gmail.com>
2021-12-11 00:56:09 +0600

    text field added

--- on_time_app/lib/main.dart
+++ on_time_app/lib/main.dart
@@ -28,61 +28,99 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(28.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Stack(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //Toggle Bar//
+
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff3C1F7B),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      width: 250,
-                      height: 40,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 20.0),
-                          child: Text(
-                            "Schedule",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
+                    Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xff3C1F7B),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          width: 250,
+                          height: 40,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20.0),
+                              child: Text(
+                                "Schedule",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 125.0, top: 4),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xff0E091A),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            width: 120,
+                            height: 32,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 38.0, top: 5),
+                              child: Text(
+                                "Notes",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 125.0, top: 4),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xff0E091A),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        width: 120,
-                        height: 32,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 38.0, top: 5),
-                          child: Text(
-                            "Notes",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
+
+            //Text Field//
+
+            Padding(
+              padding: const EdgeInsets.only(top: 1, left: 20.0, right: 20),
+              child: Container(
+                height: 35,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Color(0xffCCC3FE)),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Icon(Icons.search),
+                    ),
+                    hintText: "Search Note",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+
+            //Card//
+
+            Container(
+              width: 300,
+              height: 100,
+              color: Colors.amberAccent,
+            )
+          ],
         ),
       ),
     );
