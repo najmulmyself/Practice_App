c0a965e829701e68448518b5f0cb009e871fb245
Najmul Huda <najmul.myself@gmail.com>
2021-12-30 16:40:34 +0600

    setState added

--- tap_tap_game/lib/main.dart
+++ tap_tap_game/lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int countTop = 50;
+  int countBottom = 50;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,19 +23,37 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 Expanded(
-                  flex: 50,
-                  child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.teal.shade100,
+                  flex: countTop,
+                  child: GestureDetector(
+                    onTap: () {
+                      print("tapped top");
+                      setState(() {
+                        countTop = countTop + 4;
+                        countBottom = countBottom - 4;
+                      });
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Colors.teal.shade100,
+                    ),
                   ),
                 ),
                 Expanded(
-                  flex: 50,
-                  child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.purple.shade100,
+                  flex: countBottom,
+                  child: GestureDetector(
+                    onTap: () {
+                      print("tapped bottom");
+                      setState(() {
+                        countBottom = countBottom + 4;
+                        countTop = countTop - 4;
+                      });
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Colors.purple.shade100,
+                    ),
                   ),
                 ),
               ],
