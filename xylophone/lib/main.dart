bb24e4270bb5e07d73f73db687f89058516afffe
Najmul Huda <najmul.myself@gmail.com>
2022-01-03 21:10:49 +0600

    7 btn added

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,14 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(Xylophone());
 
-class XylophoneApp extends StatelessWidget {
+class Xylophone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play("note1.wav");
+                  },
+                  child: Text("Click me"),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
