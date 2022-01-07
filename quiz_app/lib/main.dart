f3cc644e8ea45e8c62708ae342a8abf0597fa608
Najmul Huda <najmul.myself@gmail.com>
2022-01-08 00:18:14 +0600

    class added

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,16 +28,23 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> checkMark = [];
-  List ShowQuestions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
+  // List ShowQuestions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
   int questionNumber = 0;
-  List answer = [
-    false,
-    true,
-    true,
+  // List answer = [
+  //   false,
+  //   true,
+  //   true,
+  // ];
+  List questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   @override
@@ -51,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                ShowQuestions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,7 +85,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    bool correctAns = answer[questionNumber];
+                    bool correctAns =
+                        questionBank[questionNumber].questionAnswear;
                     if (correctAns == true) {
                       print("you got the right ans");
                     } else {
@@ -112,7 +121,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    bool correctAns = answer[questionNumber];
+                    bool correctAns =
+                        questionBank[questionNumber].questionAnswear;
                     if (correctAns == false) {
                       print("you got the right ans");
                     } else {
---
+++ quiz_app/lib/question.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+class Question {
+  String? questionText;
+  bool? questionAnswear;
+
+  Question({String? q, bool? a}) {
+    questionText = q;
+    questionAnswear = a;
+  }
+}
