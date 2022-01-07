c702abeedb6c1715a6245eb08ab11cb151c5b1c7
Najmul Huda <najmul.myself@gmail.com>
2022-01-07 21:36:51 +0600

    question list added

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -26,19 +26,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> checkMark = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+  List<Widget> checkMark = [];
+  List ShowQuestions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
   @override
   Widget build(BuildContext context) {
@@ -52,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                ShowQuestions[1],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
