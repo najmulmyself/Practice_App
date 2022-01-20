8b76880f5c71b8ee4f1aee24a582adcdb4519778
Najmul Huda <najmul.myself@gmail.com>
2022-01-20 17:49:14 +0600

    pop up not working

--- moneyfy/lib/main.dart
+++ moneyfy/lib/main.dart
@@ -1,17 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:moneyfy/models/transaction.dart';
 import 'package:intl/intl.dart';
-import 'package:moneyfy/widgets/user_transaction.dart';
+import 'package:moneyfy/widgets/new_transaction.dart';
+import 'package:moneyfy/widgets/transaction_list.dart';
+// import 'package:moneyfy/widgets/user_transaction.dart';
 // import 'package:moneyfy/widgets/transaction_list.dart';
 // import 'package:moneyfy/widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // String? title;
-  // String? amount;
-  // final title = TextEditingController();
-  // final amount = TextEditingController();
+  final List _userTransactions = [
+    Transactions(
+      id: 1,
+      title: "New Shoes",
+      amount: 60.22,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 2,
+      title: "Grocery",
+      amount: 40.15,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 3,
+      title: "Shirt",
+      amount: 50.15,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 4,
+      title: "Shorts",
+      amount: 55.15,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transactions(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          // we can pass underscore (_) in builder.
+          return NewTransaction(_addNewTransaction);
+
+          // name of the widget we want to show in the pop up menu. which is NewTransaction()
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +77,7 @@ class MyApp extends StatelessWidget {
           title: Text('Material App Bar'),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () => _startAddNewTransaction(context),
               icon: Icon(Icons.add),
             ),
           ],
@@ -35,13 +90,14 @@ class MyApp extends StatelessWidget {
               Card(
                 child: Text("Hello"),
               ),
-              UserTransaction(),
+              // UserTransaction(),
+              TransactionList(_userTransactions),
             ],
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () => _startAddNewTransaction(context),
           child: Icon(Icons.add),
         ),
       ),
