9fdc5efc2e5fb7c89d2f44992fb404b901235c86
Najmul Huda <najmul.myself@gmail.com>
2022-01-22 23:46:52 +0600

    flex used

---
+++ moneyfy/lib/flex.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Flex extends StatelessWidget {
+  const Flex({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.black,
+    );
+  }
+}
